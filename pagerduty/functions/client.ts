a7841812ef2954a203403953cadbeb956462e70f
Corentin Brossault <corentin.brossault@bearer.sh>
2019-04-01 12:07:42 +0200

    feat: firstFunction

--- pagerduty/functions/client.ts
+++ pagerduty/functions/client.ts
@@ -2,13 +2,13 @@ import axios from 'axios'
 
 export default function(token: string) {
   const headers = {
-    'Accept': 'application/json',
+    Accept: 'application/vnd.pagerduty+json;version=2',
     'User-Agent': 'Bearer',
-    'Authorization': token
+    Authorization: `Token token=${token}`
   }
 
   return axios.create({
-    baseURL: 'https://api.example.com/v1',
+    baseURL: 'https://api.pagerduty.com/',
     timeout: 5000,
     headers
   })
---
+++ pagerduty/functions/firstFunction.ts
@@ -0,0 +1,37 @@
+/**
+ * This function lists the latest incidents (problems or issues) associated with the authenticated account.
+ *
+ * @context event.context.authAccess.apiKey: string
+ * @returns Incidents[]
+ */
+
+import { TAPIKEYAuthContext, FetchData, TFetchActionEvent, TFetchPromise } from '@bearer/functions'
+import Client from './client'
+
+export default class FirstFunctionFunction extends FetchData
+  implements FetchData<ReturnedData, any, TAPIKEYAuthContext> {
+  async action(event: TFetchActionEvent<Params, TAPIKEYAuthContext>): TFetchPromise<ReturnedData> {
+    try {
+      const apiKey = event.context.authAccess.apiKey
+      const client = Client(apiKey)
+      const response = await client.get('/incidents')
+
+      return { data: response.data.incidents }
+    } catch (error) {
+      return { error: 'An error has occured, ' + error.toString() }
+    }
+  }
+
+  // Uncomment the line above if you don't want your function to be called from the frontend
+  // static backendOnly = true
+}
+
+/**
+ * Typing
+ */
+
+export type Params = never
+
+export type ReturnedData = {
+  data: []
+}
