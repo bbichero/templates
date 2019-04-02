9bdabb7bb4e3eb575fd47aee054b084cb48bae75
Corentin Brossault <corentin.brossault@bearer.sh>
2019-04-02 12:51:27 +0200

    feat: firstFunction

--- hubspot/functions/client.ts
+++ hubspot/functions/client.ts
@@ -2,13 +2,13 @@ import axios from 'axios'
 
 export default function(token: string) {
   const headers = {
-    'Accept': 'application/json',
+    Accept: 'application/json',
     'User-Agent': 'Bearer',
-    'Authorization': `token ${token}`
+    Authorization: `Bearer ${token}`
   }
 
   return axios.create({
-    baseURL: 'https://api.example.com',
+    baseURL: 'https://api.hubapi.com',
     timeout: 5000,
     headers
   })
---
+++ hubspot/functions/firstFunction.ts
@@ -0,0 +1,58 @@
+/**
+ * This function lists all the contacts that have been created in Hubspot portal.
+ *
+ * @context ACCESS_TOKEN
+ * @returns Contact[]
+ */
+
+import { TOAUTH2AuthContext, FetchData, TFetchActionEvent, TFetchPromise } from '@bearer/functions'
+import Client from './client'
+
+export default class FirstFunctionFunction extends FetchData
+  implements FetchData<ReturnedData, any, TOAUTH2AuthContext> {
+  async action(event: TFetchActionEvent<Params, TOAUTH2AuthContext>): TFetchPromise<ReturnedData> {
+    try {
+      const token = event.context.authAccess.accessToken
+      const hubspot = Client(token)
+      const { data } = await hubspot.get('/contacts/v1/lists/all/contacts/all')
+
+      const contacts = (data.contacts || []).map(contact => {
+        const properties = {
+          // Default values
+          firstname: { value: '' },
+          lastname: { value: '' },
+          company: { value: '' },
+
+          // API values override default (if exist)
+          ...(contact.properties || {})
+        }
+
+        return {
+          firstname: properties.firstname.value,
+          lastname: properties.lastname.value,
+          company: properties.company.value
+        }
+      })
+      return { data: contacts }
+    } catch (error) {
+      console.error(error)
+      return error
+    }
+  }
+
+  // Uncomment the line above if you don't want your function to be called from the frontend
+  // static backendOnly = true
+}
+
+/**
+ * Typing
+ */
+export type Params = {}
+
+type Contact = {
+  firstname: string
+  lastname: string
+  company: string
+}
+
+export type ReturnedData = Contact[]
