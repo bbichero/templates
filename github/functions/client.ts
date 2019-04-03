7d5337eac024354de10d2c5cb864c761c73c3e84
Corentin Brossault <corentin.brossault@bearer.sh>
2019-04-03 12:13:08 +0200

    feat: firstFunction

--- github/functions/client.ts
+++ github/functions/client.ts
@@ -2,13 +2,13 @@ import axios from 'axios'
 
 export default function(token: string) {
   const headers = {
-    'Accept': 'application/json',
+    Accept: 'application/json',
     'User-Agent': 'Bearer',
-    'Authorization': `token ${token}`
+    Authorization: `token ${token}`
   }
 
   return axios.create({
-    baseURL: 'https://api.example.com',
+    baseURL: 'https://api.github.com',
     timeout: 5000,
     headers
   })
---
+++ github/functions/firstFunction.ts
@@ -0,0 +1,50 @@
+/**
+ * This function lists authenticated user repositories
+ *
+ * @context authAccess
+ * @returns Repository[]
+ */
+
+import { TOAUTH2AuthContext, FetchData, TFetchActionEvent, TFetchPromise } from '@bearer/functions'
+import Client from './client'
+
+export default class FirstFunctionFunction extends FetchData
+  implements FetchData<ReturnedData, any, TOAUTH2AuthContext> {
+  async action(event: TFetchActionEvent<Params, TOAUTH2AuthContext>): TFetchPromise<ReturnedData> {
+    try {
+      const token = event.context.authAccess.accessToken
+      const github = Client(token)
+      const { data } = await github.get('/user/repos')
+
+      const repositories = (data || []).map(repo => {
+        return {
+          id: repo.id,
+          name: repo.name,
+          url: repo.html_url,
+          private: repo.private
+        }
+      })
+      return { data: repositories }
+    } catch (error) {
+      console.error(error)
+      return { error }
+    }
+  }
+
+  // Uncomment the line below if you don't want your function to be called from the frontend
+  // static backendOnly = true
+}
+
+/**
+ * Typing
+ */
+export type Params = {}
+
+type Repository = {
+  id: string
+  name: string
+  url: string
+  private: boolean
+}
+
+export type ReturnedData = Repository[]
