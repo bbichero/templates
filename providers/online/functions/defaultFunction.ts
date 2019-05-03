e29cd3f532ca13d5fc3be52bf1b872be67e297a9
Corentin Brossault <corentin.brossault@bearer.sh>
2019-05-03 09:42:50 +0200

    feat: new hint + formatting

--- providers/online/functions/defaultFunction.ts
+++ providers/online/functions/defaultFunction.ts
@@ -1,5 +1,5 @@
 /**
- * This function lists all safes from online c14 storage
+ * This function lists all safes from Online.net c14 storage.
  *
  * @context event.context.auth.accessToken: string
  * @returns Safes[] | error
@@ -9,7 +9,7 @@ import { TOAUTH2AuthContext, FetchData, TFetchActionEvent, TFetchPromise } from 
 import Client from './client'
 
 export default class DefaultFunctionFunction extends FetchData
-implements FetchData<ReturnedData, any, TOAUTH2AuthContext> {
+  implements FetchData<ReturnedData, any, TOAUTH2AuthContext> {
   async action(event: TFetchActionEvent<Params, TOAUTH2AuthContext>): TFetchPromise<ReturnedData> {
     try {
       const token = event.context.auth.accessToken
@@ -20,7 +20,7 @@ implements FetchData<ReturnedData, any, TOAUTH2AuthContext> {
       const safes = (data || []).map(safe => {
         return {
           name: safe.name,
-          status: safe.status,
+          status: safe.status
         }
       })
 
@@ -37,11 +37,11 @@ implements FetchData<ReturnedData, any, TOAUTH2AuthContext> {
 /**
  * Typing
  */
+
 export type Params = {}
+export type ReturnedData = Safes[]
 
 export type Safes = {
   name: string
   status: string
 }
-
-export type ReturnedData = Safes[]
